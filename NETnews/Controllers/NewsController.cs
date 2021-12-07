44552c25391e1fa7e3176bd47276e5dfd4ed1c18
Lucas Lazogue <lucas.lazogue@hotmail.com>
2021-12-06 22:28:15 -0300

    Added loadNews on NewsService

---
+++ NETnews/Controllers/NewsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NETnews.Data.Services;
+using RestSharp;
+
+namespace NETnews.Controllers {
+    public class NewsController : Controller {
+
+        public readonly INewsService newsService;
+
+        public NewsController(INewsService newsService) {
+            this.newsService = newsService;
+        }
+    public IActionResult Index() {
+            newsService.loadNews();
+            return View();
+        }
+    }
+}
