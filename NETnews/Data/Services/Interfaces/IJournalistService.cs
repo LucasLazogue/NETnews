3122da4e7f7330626a4b45d89bbc68b6765a324d
Lucas Lazogue <lucas.lazogue@hotmail.com>
2021-12-14 14:49:54 -0300

    added details to journalist and sources

---
+++ NETnews/Data/Services/Interfaces/IJournalistService.cs
@@ -0,0 +1,9 @@
+﻿using NETnews.Models;
+using System.Collections.Generic;
+
+namespace NETnews.Data.Services.Interfaces {
+    public interface IJournalistService {
+        public List<News> getNewsBySource(string source);
+        public List<News> getNewsByJournalist(int id);
+    }
+}
